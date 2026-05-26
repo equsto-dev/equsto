bcb0e81c660dffb8d4b0d4b7a9a11e44ad78cc7c
equsto-dev <jurnaldang@gmail.com>
2026-05-26 21:23:16 +0300

    style(header): EQUSTO logosu bir %10 daha kucultuldu

--- EQUSTO-WORK/E-TICARET/site/public/equsto-logo.js
+++ EQUSTO-WORK/E-TICARET/site/public/equsto-logo.js
@@ -38,7 +38,7 @@
     return (
       '<img src="' +
       src +
-      '" alt="EQUSTO" class="eq-logo-img eq-logo-wordmark" width="454" height="82" decoding="async" fetchpriority="high">'
+      '" alt="EQUSTO" class="eq-logo-img eq-logo-wordmark" width="409" height="74" decoding="async" fetchpriority="high">'
     );
   }
 
