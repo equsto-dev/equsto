b4c35838286067e9eacb838a22c331e34d7514c6
equsto-dev <jurnaldang@gmail.com>
2026-05-28 00:04:26 +0300

    chore: market-reyon katalog cache bust (galeri 11)

--- E-TICARET/site/public/eq-market-reyon.js
+++ E-TICARET/site/public/eq-market-reyon.js
@@ -4,7 +4,7 @@
 ;(function () {
   "use strict";
 
-  var CATALOG_V = "20260527proso1";
+  var CATALOG_V = "20260527caglayan-gallery11";
 
   window.EqMarketReyon = {
     loadCatalog: function () {
