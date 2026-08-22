5f6fd259bf3207ae7b58057ed420cfb3f86baf71
equsto-dev <mail@equsto.com>
2026-08-22 23:53:49 +0300

    feat(catalog): Brema urun gorselleri + teknik icerik ve arama entegrasyonu
    47 Brema urunune PDF'ten cikarilan gorseller (images/catalog/brema/), teknik_ozellikler, description/aciklama, olculer eklendi; fiyat alanlari degismedi. ekipmanlar.json'a 47 Brema satiri (arama kaynagi). Meilisearch synonym kroom->brema. CATALOG_V bump.

--- E-TICARET/site/public/eq-shop-catalog-bootstrap.js
+++ E-TICARET/site/public/eq-shop-catalog-bootstrap.js
@@ -6,7 +6,7 @@
 ;(function () {
   "use strict";
 
-  var CATALOG_V = "20260822-brema-kroom-fiyat";
+  var CATALOG_V = "20260822-brema-kroom-icerik";
   var __fullMem = null;
   var __fullInflight = null;
   var __deptMem = Object.create(null);
